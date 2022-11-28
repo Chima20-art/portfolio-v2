ea30ba5dea060c6a9544a36156b11bec7f3473e2
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2022-11-28 19:20:34 +0100

    added links to header buttons

--- components/header.js
+++ components/header.js
@@ -1,10 +1,11 @@
 import { motion } from 'framer-motion'
+import Link from 'next/link'
 import { useState } from 'react'
 
 export default function Header() {
     const pages = [
         { title: 'Home', url: '/' },
-        { title: 'About', url: '/' },
+        { title: 'About', url: '/about' },
         { title: 'Portfolio', url: '/' },
         { title: 'Service', url: '/' },
         { title: 'News', url: '/' },
@@ -13,39 +14,42 @@ export default function Header() {
     const [hoverdItem, setHoveredItem] = useState('')
 
     return (
-        <div className="h-14 md:h-14 md:max-w-[92%] mx-auto flex items-center justify-between font-medium ">
+        <div className="h-14 md:h-14  flex items-center justify-between font-medium ">
             <div className="  uppercase font-bold text-[18px] ">Michich</div>
             <div className=" flex  text-grey text-sm ">
                 {pages?.map((page) => {
                     return (
-                        <div
-                            onMouseEnter={() => setHoveredItem(page.title)}
-                            onMouseLeave={() => setHoveredItem('')}
-                            key={page.title}
-                            className=" relative  hover:cursor-pointer  overflow-hidden hover:text-white text-grey  px-[17px] py-[9px]  font-medium   "
-                        >
-                            <motion.div
-                                initial={{
-                                    x: '-105%',
-                                }}
-                                animate={
-                                    hoverdItem == page.title
-                                        ? {
-                                              x: '0%',
-                                          }
-                                        : { x: '-105%' }
-                                }
-                                transition={{
-                                    duration: 0.4,
-                                    delay: 0.2,
-                                    ease: 'easeIn',
-                                }}
-                                className="bg-grey h-full w-full absolute right-0 top-0 -z-20 "
-                            ></motion.div>
-                            <p className="  z-[10] w-full h-full   mx-auto h-full bg-transparent  ">
-                                {page.title}
-                            </p>
-                        </div>
+                        <Link href={page.url}>
+                            {' '}
+                            <div
+                                onMouseEnter={() => setHoveredItem(page.title)}
+                                onMouseLeave={() => setHoveredItem('')}
+                                key={page.title}
+                                className=" relative  hover:cursor-pointer  overflow-hidden hover:text-white text-grey  px-[17px] py-[9px]  font-medium   "
+                            >
+                                <motion.div
+                                    initial={{
+                                        x: '-105%',
+                                    }}
+                                    animate={
+                                        hoverdItem == page.title
+                                            ? {
+                                                  x: '0%',
+                                              }
+                                            : { x: '-105%' }
+                                    }
+                                    transition={{
+                                        duration: 0.4,
+                                        delay: 0.2,
+                                        ease: 'easeIn',
+                                    }}
+                                    className="bg-grey h-full w-full absolute right-0 top-0 -z-20 "
+                                ></motion.div>
+                                <p className="  z-[10] w-full h-full   mx-auto h-full bg-transparent  ">
+                                    {page.title}
+                                </p>
+                            </div>
+                        </Link>
                     )
                 })}
             </div>
