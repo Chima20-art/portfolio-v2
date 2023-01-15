c10594e51853b6de6c4fb5aac19f5da4236dd0bf
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2023-01-15 14:33:44 +0100

    added contact form.

--- pages/contact.js
+++ pages/contact.js
@@ -25,18 +25,46 @@ export default function Contact() {
                 </p>
                 <span className="bg-grey  w-full flex-1 h-[1px]" />
             </div>
-            <div className="w-full flex   gap-8">
+            <div className="w-full flex md:flex-row flex-col   gap-8">
                 {contacts?.map((contact) => {
                     return (
-                        <div className="flex flex-col justify center align center flex-1 border border-grey-400 w-fit py-10 text-center ">
+                        <div className="flex flex-col justify center align center flex-1  border border-grey-400 w-full md:py-10 py-6 px-2 text-center ">
                             <contact.icon className=" w-full mb-4" />
-                            <p className=" text-[#7d7789] ">
+                            <p className=" text-[#7d7789] text-[12px] ">
                                 {contact.description}
                             </p>
                         </div>
                     )
                 })}
             </div>
+            <form
+                type="submit"
+                className=" flex flex-col py-8  lg:w-[80%] w-full text-start"
+            >
+                <input
+                    type="text"
+                    placeholder="Name"
+                    className="border border-grey-300 py-3  px-2 w-full mb-6"
+                />
+                <input
+                    type="email"
+                    placeholder="Email"
+                    className="border border-grey-300 py-3 px-2 w-full  mb-6"
+                />
+                <textarea
+                    type="text"
+                    placeholder="Message"
+                    cols="60"
+                    rows="3"
+                    className="border border-grey-300 py-3 px-2 w-full cols  "
+                />
+                <button
+                    type="button"
+                    className="capitalize bg-[#333333] text-white py-4 mt-6 w-fit px-8 text-[14px] font-[600] hover:bg-white hover:text-black hover:border hover:border-black "
+                >
+                    send message
+                </button>
+            </form>
         </div>
     )
 }
