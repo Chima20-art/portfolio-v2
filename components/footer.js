f2e1f7eb8c23d990b7ea28ace07a7ed98fca4cab
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2023-01-17 16:12:36 +0100

    added socialmedia links.

--- components/footer.js
+++ components/footer.js
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import { FaFacebookF, FaLinkedin } from 'react-icons/fa'
 import { RiInstagramFill } from 'react-icons/ri'
 import { VscGithubInverted } from 'react-icons/vsc'
@@ -7,10 +8,17 @@ export default function Footer() {
         <div className=" h-14 flex justify-between  py-6 ">
             <p className="text-grey font-[400] text-[14px]">Copyright @2022</p>
             <div className="flex justify-center align-center text-black  ">
-                <FaFacebookF className="w-[16px] h-[14px] my-auto mr-2 cursor-pointer" />
-                <RiInstagramFill className="w-[16px] h-[16px] my-auto mr-2 cursor-pointer " />
-                <FaLinkedin className="w-[16px] h-[16px] my-auto mr-2 cursor-pointer" />
-                <VscGithubInverted className="w-[16px] h-[14px] my-auto cursor-pointer" />
+                <Link href="https://www.instagram.com/minan_sha/">
+                    {' '}
+                    <RiInstagramFill className="w-[16px] h-[16px] my-auto mr-2 cursor-pointer " />
+                </Link>
+
+                <Link href="https://www.linkedin.com/in/chaimae-michich-0bb9a11a1/">
+                    <FaLinkedin className="w-[16px] h-[16px] my-auto mr-2 cursor-pointer" />
+                </Link>
+                <Link href="https://github.com/Chima20-art">
+                    <VscGithubInverted className="w-[16px] h-[14px] my-auto cursor-pointer" />
+                </Link>
             </div>
         </div>
     )
