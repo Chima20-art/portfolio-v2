e334937577286570f6d08349e6dc79b2be4ff8f3
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2023-01-18 16:27:46 +0100

    fixed mobile menu.

--- components/header.js
+++ components/header.js
@@ -1,6 +1,9 @@
 import { AnimatePresence, motion } from 'framer-motion'
 import Link from 'next/link'
 import { useState } from 'react'
+import { FaLinkedin } from 'react-icons/fa'
+import { RiInstagramFill } from 'react-icons/ri'
+import { VscGithubInverted } from 'react-icons/vsc'
 
 export default function Header() {
     const pages = [
@@ -99,68 +102,86 @@ export default function Header() {
                     </svg>
                 </div>
             </div>
+
             <AnimatePresence>
                 {showMobileMenu && (
-                    <motion.div
-                        initial={{
-                            y: '-100%',
-                            opacity: 0,
-                        }}
-                        animate={{
-                            y: '0%',
-                            opacity: 1,
-                        }}
-                        exit={{
-                            x: '-100%',
-                            opacity: 0,
-                        }}
-                        transition={{
-                            duration: 0.8,
-                        }}
-                        className="px-[4%] bg-red-300 w-screen h-screen fixed flex flex-col z-50 top-0 left-0"
-                    >
-                        <div className="flex h-14 py-4  items-center justify-between font-medium ">
-                            <Link
-                                href="/"
-                                className="  uppercase font-bold  text-[18px] text-black hover:scale-110 "
-                            >
-                                Michich
-                            </Link>
-                            <div
-                                onClick={() => setShowMobileMenu(false)}
-                                className="flex lg:hidden  "
-                            >
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    strokeWidth={1.5}
-                                    stroke="currentColor"
-                                    className="w-10 h-10 cursor-pointer hover:scale-110 active:scale-125"
+                    <div className="bg-red-700 ">
+                        <motion.div
+                            initial={{
+                                x: '100%',
+                                opacity: 0,
+                            }}
+                            animate={{
+                                x: '0%',
+                                opacity: 1,
+                            }}
+                            exit={{
+                                x: '100%',
+                                opacity: 0,
+                            }}
+                            transition={{
+                                duration: 0.8,
+                            }}
+                            className="px-[4%]  w-full bg-white h-screen align-right fixed flex flex-col z-50 top-0 left-0 "
+                        >
+                            <div className="flex h-14 py-4   items-center justify-between font-medium ">
+                                <Link
+                                    href="/"
+                                    className="  uppercase font-bold  text-[18px] text-black hover:scale-110 "
                                 >
-                                    <path
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        d="M6 18L18 6M6 6l12 12"
-                                    />
-                                </svg>
-                            </div>
-                        </div>
-                        <div className="flex-1 flex flex-col items-center justify-center gap-4">
-                            {pages?.map((item) => {
-                                return (
-                                    <Link
-                                        onClick={() => setShowMobileMenu(false)}
-                                        href={item.url}
-                                        className=" text-2xl min-w-[30vw] text-center hover:underline hover:font-bold"
-                                        key={item?.title}
+                                    Michich
+                                </Link>
+                                <div
+                                    onClick={() => setShowMobileMenu(false)}
+                                    className="flex lg:hidden  "
+                                >
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        strokeWidth={1.5}
+                                        stroke="currentColor"
+                                        className="w-10 h-10 cursor-pointer hover:scale-110 active:scale-125"
                                     >
-                                        {item?.title}
-                                    </Link>
-                                )
-                            })}
-                        </div>
-                    </motion.div>
+                                        <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            d="M6 18L18 6M6 6l12 12"
+                                        />
+                                    </svg>
+                                </div>
+                            </div>
+                            <div className="flex-1 flex flex-col items-center justify-center gap-4">
+                                {pages?.map((item) => {
+                                    return (
+                                        <Link
+                                            onClick={() =>
+                                                setShowMobileMenu(false)
+                                            }
+                                            href={item.url}
+                                            className=" text-2xl min-w-[30vw] text-start hover:underline hover:font-bold"
+                                            key={item?.title}
+                                        >
+                                            {item?.title}
+                                        </Link>
+                                    )
+                                })}
+                            </div>
+                            <div className="flex justify-center align-center text-black py-4 ">
+                                <Link href="https://www.instagram.com/minan_sha/">
+                                    {' '}
+                                    <RiInstagramFill className="w-[24px] h-[24px] my-auto mr-2 cursor-pointer " />
+                                </Link>
+
+                                <Link href="https://www.linkedin.com/in/chaimae-michich-0bb9a11a1/">
+                                    <FaLinkedin className="w-[24px] h-[24px] my-auto mr-2 cursor-pointer" />
+                                </Link>
+                                <Link href="https://github.com/Chima20-art">
+                                    <VscGithubInverted className="w-[24px] h-[24px] my-auto cursor-pointer" />
+                                </Link>
+                            </div>
+                        </motion.div>
+                    </div>
                 )}
             </AnimatePresence>
         </>
