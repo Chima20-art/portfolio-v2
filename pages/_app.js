8c3036b303e00056ef6b21899d8a30cdc41541c3
chaimae michich <michichchaimae@gmail.com>
2023-10-08 12:24:19 +0100

    finailized pop up cookie

--- pages/_app.js
+++ pages/_app.js
@@ -9,15 +9,60 @@ import { DefaultSeo } from "next-seo";
 import Image from "next/image";
 import GoogleAnalytics from "../components/googleAnalytics";
 import CookiePopUp from "../components/cookiePopUp";
+import { useCookies } from "react-cookie";
+
+const ANALYTICS_COOKIES = ["_ga", "1P_JAR", "_ga_R7JKCQTMRS"];
+const ESSENTIAL_COOKIES = ["user-id"];
 
 function MyApp({ Component, pageProps, router }) {
   const [loading, setLoading] = useState(true);
+  const [cookies, setCookie, removeCookie] = useCookies(["cookie-name"]);
+  const [acceptedCookies, setAcceptedCookies] = useState("essential"); // eithe or essential 'analytics' or 'all'
+  const [hideBanenr, setHideBanner] = useState(false);
+
+  const setCookiePolicy = (value) => {
+    setAcceptedCookies(value);
+    setHideBanner(true);
+    localStorage.setItem("setShowBanner", true);
+  };
+
+  useEffect(() => {
+    console.log("cookies ");
+    console.log(JSON.stringify(cookies));
+    let cookiesKeys = Object.keys(cookies);
+    cookiesKeys.forEach((key) => {
+      console.log("dealing with cookie ", key);
+      if (ANALYTICS_COOKIES.includes(key) && acceptedCookies == "essential") {
+        console.log("removed cookie");
+        removeCookie(key);
+      } else {
+        console.log("keeping cookie");
+      }
+    });
+    console.log(" ");
+  }, [cookies]);
 
   useEffect(() => {
     setTimeout(() => {
       setLoading(false);
     }, 1000);
+    setCookie("user-id", "user-41");
+    let pressiedCookie = localStorage.getItem("acceptedCookies");
+    if (
+      ["all", "essential", "analytics"].includes(pressiedCookie) &&
+      acceptedCookies != pressiedCookie
+    ) {
+      setAcceptedCookies(pressiedCookie);
+    }
+    let ShouldShowBanner = localStorage.getItem("setShowBanner");
+    if (ShouldShowBanner) {
+      setHideBanner(true);
+    }
   }, []);
+
+  useEffect(() => {
+    localStorage.setItem("acceptedCookies", acceptedCookies);
+  }, [acceptedCookies]);
 
   return (
     <>
@@ -33,18 +78,19 @@ function MyApp({ Component, pageProps, router }) {
           ],
         }}
       />
-      {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS ? (
+      {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS &&
+      (acceptedCookies == "all" || acceptedCookies == "analytics") ? (
         <GoogleAnalytics ga_id={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS} />
-      ) : (
-        <GoogleAnalytics ga_id={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS} />
-      )}
+      ) : null}
       <div className="w-screen px-[4%] w-full h-screen mx-auto flex flex-col scrollbar-hide  text-grey ">
         {loading && <Loading />}
 
         <Header />
 
         <div className=" relative w-full flex flex-1 h-[calc(100vh_-_112px)]  justify-start  object-contain scrollbar-hide ">
-          <CookiePopUp />
+          {!hideBanenr && !loading && (
+            <CookiePopUp setAcceptedCookies={setCookiePolicy} />
+          )}
           <div className=" xl:flex hidden  w-[40%]">
             <Image
               height={600}
